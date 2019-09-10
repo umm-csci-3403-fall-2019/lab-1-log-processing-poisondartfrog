2c337bcd391cdd17936ad85814af176bf7d1ec9a
Melissa Kamrowski <kamro010@athena.morris.umn.edu>
2019-09-10 11:39:35 -0500

    Initialize process_logs.sh, create_username_dist.sh; Begin fixing errors in files

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -5,14 +5,25 @@ DIR=$1
 cd "$DIR" || exit
 
 
-for f in *.tgz
-do
-	output_dir=client_logs_$(basename $f "_secure.tgz")
-	mkdir "$output_dir"
-	tar -zxf "$f" --directory "$output_dir"
-done
+#for f in *.tgz
+#do
+#	output_dir=client_logs_$(basename $f "_secure.tgz")
+#	mkdir "$output_dir"
+#	tar -zxf "$f" --directory "$output_dir"
+#done
 
-securedir="../log_files/client_logs_cscirepo/var/log/secure"
+#for f in client_logs_*
+#do
+#	SECDIR=$f/var/log/
+#
+#	for t in secure*
+#	do
+#		awk '/Failed/ { print > "holding.txt" }' $SECDIR$t
+#		echo $(pwd)
+#	done
+#
+#done
 
-awk '/Failed/ { print }' $securedir
 
+sed -En 's/([a-zA-Z0-9 ]+):...+ for ([a-zA-Z0-9_-]+) from ([0-9\.]+)...+/\1 \2 \3/p' < holding.txt > failed_login_data.txt
+sed -En 's/([a-zA-Z0-9 ]+):...+ for invalid user ([a-zA-Z0-9_-]+) from ([0-9\.]+)...+/\1 \2 \3/p' < holding.txt >> failed_login_data.txt
--- bin/process_logs.sh
+++ bin/process_logs.sh
@@ -1,6 +1,17 @@
-#!usr/bin/env bash
+#!/usr/bin/env bash
+
+SCRATCH=$(mktemp --directory)
+
+cd ../log_files || exit
+
+for f in *.tgz
+do
+	output_dir=client_logs_$(basename $f "_secure.tgz")
+	mkdir "$output_dir"
+	tar -zxf "$f" --directory "$SCRATCH/$output_dir"
+done
 
 
 
 
-
+rm -r $SCRATCH
