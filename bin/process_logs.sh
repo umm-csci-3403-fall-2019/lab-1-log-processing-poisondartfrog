58ac5a06701a5e03ea177c041a30834bb0b4033c
Ananya Teklu <ananyateklu@x-10-133-10-2.wireless.umn.edu>
2019-09-11 14:32:45 -0500

    fix the proccess_logs.sh to take in a set of gzipped files and extracts them into scratch directory and deleted unnecessary files in log_files

--- bin/process_logs.sh
+++ bin/process_logs.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
 
 SCRATCH=$(mktemp --directory)
-
-cd ../log_files || exit
-
-for f in *.tgz
+echo $SCRATCH
+for f in $@
 do
-	output_dir=client_logs_$(basename $f "_secure.tgz")
-	mkdir "$output_dir"
-	tar -zxf "$f" --directory "$SCRATCH/$output_dir"
+    output_dir=client_logs_$(basename $f "_secure.tgz")
+    mkdir $SCRATCH/$output_dir
+    tar -zxf "$f" --directory "$SCRATCH/$output_dir"
 done
 
 
 
 
-rm -r $SCRATCH
+#rm -r $SCRATCH
