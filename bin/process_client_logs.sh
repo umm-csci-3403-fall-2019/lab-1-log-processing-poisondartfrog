707594666d0774ed44764b5396436c73e70ab04c
Melissa Kamrowski <kamro010@asimov.morris.umn.edu>
2019-09-11 19:17:33 -0500

    Fix scripts to put information into proper files
    Scripts were working but not putting information into a scratch directory, causing tests to fail. Scripts were re-written to create scratch directory and work with each other

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -5,24 +5,6 @@ DIR=$1
 cd "$DIR" || exit
 awk '/Failed/ { print > "holding.txt" }' $DIR/var/log/*
 pwd
-#for f in *.tgz
-#do
-#	output_dir=client_logs_$(basename $f "_secure.tgz")
-#	mkdir "$output_dir"
-#	tar -zxf "$f" --directory "$output_dir"
-#done
-
-#for f in client_logs_*
-#do
-#	SECDIR=$f/var/log/
-#
-#	for t in secure*
-#	do
-#		awk '/Failed/ { print > "holding.txt" }' $SECDIR$t
-#		echo $(pwd)
-#	done
-#
-#done
 
 
 sed -En 's/([a-zA-Z0-9 ]+):...+ for ([a-zA-Z0-9_-]+) from ([0-9\.]+)...+/\1 \2 \3/p' < holding.txt > failed_login_data.txt
--- bin/process_logs.sh
+++ bin/process_logs.sh
@@ -1,21 +1,23 @@
 #!/usr/bin/env bash
+
+
 SCRATCH=$(mktemp --directory)
+
 echo $SCRATCH
+cd ../log_files
+
 for f in $*
 do
     output_dir=$(basename $f "_secure.tgz")
     mkdir $SCRATCH/$output_dir
     tar -zxf "$f" --directory "$SCRATCH/$output_dir"
-    SECDIR=$output_dir/var/log/
-    ./process_client_logs.sh $SCRATCH/$output_dir
-#   awk '/Failed/ { print > "holding.txt" }' $SCRATCH
-#    for t in $SECDIR/secure*
-#    do
-#	
-#    awk '/Failed/ { print > "holding.txt" }' $SCRATCH/$t
-#    done
+#    SECDIR=$output_dir/var/log/
+
+    ../bin/process_client_logs.sh $SCRATCH/$output_dir
+
     
 done
+
 
 
 
