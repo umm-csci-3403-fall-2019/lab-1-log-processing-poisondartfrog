0f9e711c8655b685852ff7c7da0b76bbb7f1bd39
Melissa Kamrowski <kamro010@asimov.morris.umn.edu>
2019-09-11 21:55:42 -0500

    Implement create_username_dist.sh script

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -1,3 +1,19 @@
-#!usr/bin/env bash
+#!/usr/bin/env bash
+
+DIR=$1
+
+for f in $(find $DIR -name failed_login_data.txt)
+do
 
 
+	sed -En 's/[a-zA-Z ]+[0-9 ]+[ 0-9]+([a-zA-Z0-9\-_]+)...+/\1/p' < $f >> $DIR/only_usrs.txt
+	
+done
+
+
+sort < $DIR/only_usrs.txt > $DIR/sorted.txt
+uniq -c < $DIR/sorted.txt > $DIR/uniqsort.txt
+	
+sed -E 's/([0-9]+) ([a-zA-Z0-9_\-]+)/data.addRow([\x27\2\x27, \1]);/' < $DIR/uniqsort.txt >> $DIR/datalist.txt
+	
+./bin/wrap_contents.sh $DIR/datalist.txt html_components/username_dist $DIR/username_dist.html
--- bin/process_logs.sh
+++ bin/process_logs.sh
@@ -4,19 +4,18 @@
 SCRATCH=$(mktemp --directory)
 
 echo $SCRATCH
-cd ../log_files
+cd ../log_files || exit
 
 for f in $*
 do
     output_dir=$(basename $f "_secure.tgz")
     mkdir $SCRATCH/$output_dir
     tar -zxf "$f" --directory "$SCRATCH/$output_dir"
-#    SECDIR=$output_dir/var/log/
-
     ../bin/process_client_logs.sh $SCRATCH/$output_dir
-
-    
+   
 done
+
+../bin/create_username_dist.sh $SCRATCH
 
 
 
