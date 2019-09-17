5a013dfc2f3606d96ec72099a31383beae70f18a
Melissa Kamrowski <kamro010@athena.morris.umn.edu>
2019-09-17 11:24:55 -0500

    Complete create_country_dist.sh to pass all tests and work. i Please enter the commit message for your changes. Lines starting

--- bin/create_country_dist.sh
+++ bin/create_country_dist.sh
@@ -8,10 +8,17 @@ do
     sed -En 's/...+ ([[:digit:]\.]+)/\1/p'< $f >> $DIR/onlyip.txt
 done
 
+sort <$DIR/onlyip.txt > $DIR/sortedip.txt
+join $DIR/sortedip.txt etc/country_IP_map.txt > $DIR/countrycode.txt
+sed -E 's/.+ ([A-Z]+)/\1/g' $DIR/countrycode.txt \
+   | sort \
+   | uniq -c \
+   > $DIR/uniqsortcc.txt
+
+
+sed -E 's/[ ]+ (.+) ([A-Z]+)/data.addRow([\x27\2\x27, \1]);/' < $DIR/uniqsortcc.txt > $DIR/datalistcc.txt
+
+bin/wrap_contents.sh $DIR/datalistcc.txt html_components/country_dist $DIR/country_dist.html
 
 
 
-#...+ (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})
-
-
-#[a-zA-Z ]+[0-9 ]+[a-z-A-Z0-9-_ ]+( \d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})
